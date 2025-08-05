ce4277fe9510e73f4301397241d5a45a8c910168
PaulaStael <paula.stael@estudante.ufjf.br>
2025-08-05 15:00:16 -0300

    Recuperando alterações do stash

---
+++ Parametros.py
@@ -0,0 +1,30 @@
+import numpy as np
+import scipy.signal as signal
+
+# Parâmetros do Boost
+Vin = 48            # Tensão de entrada (V)
+L = 400e-6          # Indutância (H)
+C = 47e-6           # Capacitância (F)
+R = 5               # Carga resistiva (Ohms)
+fs = 20e3           # Frequência de chaveamento (Hz)
+Ts = 1/fs           # Período de amostragem (s)
+
+# Função de transferência contínua: G(s) = Vin / (L*C*s^2 + (R/L)*s)
+num_ct = [Vin]
+den_ct = [L*C, R, 0]
+
+# Discretização com método bilinear
+system_dt = signal.cont2discrete((num_ct, den_ct), Ts, method='bilinear')
+b = system_dt[0].flatten()  # Numerador
+a = system_dt[1].flatten()  # Denominador
+
+# Arredondar para 8 casas e formatar com 'f'
+b = np.round(b, 8)
+a = np.round(a, 8)
+
+# Gerar coeficientes
+print(f"B0 {b[0]:.8f}f")
+print(f"B1 {b[1]:.8f}f")
+print(f"B2 {b[2]:.8f}f")
+print(f"A1 {a[1]:+.8f}f")
+print(f"A2 {a[2]:+.8f}f")
